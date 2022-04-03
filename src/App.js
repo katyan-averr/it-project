a87ad35554f634513997a42fde041f9d9faf0f6a
katyan-averr <85882133+katyan-averr@users.noreply.github.com>
2022-04-03 18:02:48 +0800

    ниче не получается

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import Button from '@material-ui/core/Button';
-import { AppBar, Container, IconButton, Toolbar, Box } from '@material-ui/core';
+import { AppBar, Container, IconButton, Toolbar, Box, Paper, Grid, Typography } from '@material-ui/core';
 import { mergeClasses } from '@material-ui/styles';
 import MiscellaneousServicesIcon from '@mui/icons-material/MiscellaneousServices';
 import PersonIcon from '@mui/icons-material/Person';
 import {makeStyles} from '@material-ui/core/styles';
+import { style } from '@mui/system';
 
 const useStyles = makeStyles((theme) =>({
   root:{
-    flexGrow: 1
+    flexGrow:1
   },
   menuButton:{
     marginRight: theme.spacing(1)
@@ -22,22 +23,40 @@ function App(){
   const classes = useStyles();
 
   return(
+    <>
     <AppBar position="fixed">
       <Container fixed>
         <Toolbar>
-          <IconButton edge="start" color="inherit" aria-label='menu' className={classes.menuButton}>
             <MiscellaneousServicesIcon fontSize='large'/>
-          </IconButton>
           <Box mr={3}>
             <Button color="4c7bd9" variant='contained'>Каталог</Button>
           </Box>
           <Button color="inherit" variant='outlined'>Мои заявки</Button>
-          <IconButton edge="start" color="inherit" aria-label='menu' className={classes.menuButton} >
+          <IconButton edge="start" color="inherit" aria-label='menu' className='App'>
             <PersonIcon fontSize='large' />
           </IconButton>
+
         </Toolbar>
       </Container>
     </AppBar>
+    
+    <main>
+      <Paper className={classes.mainFeaturesPost} style={{backgroundImage: 'url(https://miro.medium.com/max/1200/0*rF85nhzEmjrtjoqX.png)'}}>
+        <Container fixed>
+          <Grid Container>
+            <Grid item md={6}>
+              <div className={classes.mainFeaturesPostContent}>
+                <Button variant='contained' color="4c7bd9">
+                  opa
+                </Button>
+              </div>
+            </Grid>
+          </Grid>
+        </Container>
+      </Paper>
+    </main>
+
+    </>
     );
 }
 
