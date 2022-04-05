ec98055bc115c08c35297a617a9dbaac4ca4613f
katyan-averr <85882133+katyan-averr@users.noreply.github.com>
2022-04-05 22:53:47 +0800

    сделала страничку для создания заявки и страничку со моими заявками

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
 import Button from '@material-ui/core/Button';
-import { AppBar, Container, IconButton, Toolbar, Box, Paper, Grid, Typography } from '@material-ui/core';
+import { AppBar, Container, IconButton, Toolbar, Box, Paper, Grid } from '@material-ui/core';
 import { mergeClasses } from '@material-ui/styles';
 import MiscellaneousServicesIcon from '@mui/icons-material/MiscellaneousServices';
 import PersonIcon from '@mui/icons-material/Person';
 import {makeStyles} from '@material-ui/core/styles';
-import { style } from '@mui/system';
 
 const useStyles = makeStyles((theme) =>({
   root:{
@@ -38,16 +37,17 @@ function App(){
 
   return(
     <>
+
     <AppBar position="static" style={{backgroundColor:"#A4C8EC", background:"rgba(164, 200, 236, 0.75)" }}>
       <Container fixed>
         <Toolbar>
-            <MiscellaneousServicesIcon fontSize='large'/>
+            <MiscellaneousServicesIcon fontSize='large' style={{color: "black"}}/>
           <Box mr={3}>
             <Button color="4c7bd9" variant='contained'>Каталог</Button>
           </Box>
-          <Button color="inherit" variant='outlined'>Мои заявки</Button>
+          <Button color="inherit" variant='outlined' style={{color: "black"}}>Мои заявки</Button>
           <IconButton edge="start" color="inherit" aria-label='menu' className='App'>
-            <PersonIcon fontSize='large' />
+            <PersonIcon fontSize='large' style={{color: "black"}}/>
           </IconButton>
 
         </Toolbar>
@@ -65,27 +65,27 @@ function App(){
                 </Button>
           </Grid>
           <Grid item xs={12} sm={6} md={4}>
-              <Button variant='contained' color="4c7bd9" style={{ width: "230px", height: "90px", backgroundColor:"#A4C8EC", background:"rgba(164, 200, 236, 0.75)" }}>
+              <Button variant='contained' style={{ width: "230px", height: "90px", backgroundColor:"#A4C8EC", background:"rgba(164, 200, 236, 0.75)" }}>
                   Проблема с настроением
                 </Button>
           </Grid>
           <Grid item xs={12} sm={6} md={4}>
-              <Button variant='contained' color="4c7bd9" style={{ width: "230px", height: "90px", backgroundColor:"#A4C8EC", background:"rgba(164, 200, 236, 0.75)" }}>
+              <Button variant='contained' style={{ width: "230px", height: "90px", backgroundColor:"#A4C8EC", background:"rgba(164, 200, 236, 0.75)" }}>
                   Проблема с принтером
                 </Button>
           </Grid>
           <Grid item xs={12} sm={6} md={4}>
-              <Button variant='contained' color="4c7bd9" style={{ width: "230px", height: "90px", backgroundColor:"#A4C8EC", background:"rgba(164, 200, 236, 0.75)" }}>
+              <Button variant='contained' style={{ width: "230px", height: "90px", backgroundColor:"#A4C8EC", background:"rgba(164, 200, 236, 0.75)" }}>
                   Настройка удаленного доступа
                 </Button>
           </Grid>
           <Grid item xs={12} sm={6} md={4}>
-              <Button variant='contained' color="4c7bd9" style={{ width: "230px", height: "90px", backgroundColor:"#A4C8EC", background:"rgba(164, 200, 236, 0.75)" }}>
+              <Button variant='contained' style={{ width: "230px", height: "90px", backgroundColor:"#A4C8EC", background:"rgba(164, 200, 236, 0.75)" }}>
                   Проблема с программой
                 </Button>
           </Grid>
           <Grid item xs={12} sm={6} md={4}>
-              <Button variant='contained' color="4c7bd9" style={{ width: "230px", height: "90px", backgroundColor:"#A4C8EC", background:"rgba(164, 200, 236, 0.75)" }}>
+              <Button variant='contained' style={{ width: "230px", height: "90px", backgroundColor:"#A4C8EC", background:"rgba(164, 200, 236, 0.75)" }}>
                   Другое
                 </Button>
           </Grid>
